be91feb36400e4c3c3863dc48af47cff9f77c0e0
luketto <115785443+luckybros@users.noreply.github.com>
2026-09-13 19:32:46 +0200

    UPDATE

--- Tanks/Assets/_Tanks/Scripts/Oracles/OracleSideChannel.cs
+++ Tanks/Assets/_Tanks/Scripts/Oracles/OracleSideChannel.cs
@@ -15,8 +15,8 @@ namespace Tanks.Complete
         public OracleSideChannel()
         {
             string runID = GetRunID();
-            // ChannelId = new Guid(runID);
-            ChannelId = new Guid("621f0a70-4f87-11ea-a6bf-784f4387d1f7");
+            ChannelId = new Guid(runID);
+            //ChannelId = new Guid("621f0a70-4f87-11ea-a6bf-784f4387d1f7");
         }
 
         protected override void OnMessageReceived(IncomingMessage msg)
